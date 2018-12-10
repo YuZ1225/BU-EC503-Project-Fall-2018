5a1bb3628a888f39bf33d8f1ed618a2dcff2da01
YuZ1225 <yuzhao95@bu.edu>
2018-12-10 02:09:35 -0500

    Update with pearson

--- Data processing & show/show_all_on_small_dataset.m
+++ Data processing & show/show_all_on_small_dataset.m
@@ -2,7 +2,7 @@
 
 load('small_output_baseline.mat')
 load('small_output_cos.mat')
-
+load('small_output_pearson.mat')
 load('small_output_NB.mat')
 load('small_output_PMF.mat')
 
@@ -16,11 +16,13 @@ plot(i, y1)
 hold on
 plot(i, MAE_cos, 'o-')
 hold on
+plot(i, MAE_pearson, '.-')
+hold on
 plot(i, MAE_NB, '+-')
 hold on
 plot(i, MAE_PMF, '*-')
 hold off
-legend('baseline', 'cosine', 'NB', 'PMF')
+legend('baseline', 'cosine', 'Pearson', 'NB', 'PMF')
 str = 'MAE of Different Algorithms VS Sparsity';
 title(str)
 xlabel('Sparsity')
@@ -32,11 +34,13 @@ plot(i, y2)
 hold on
 plot(i, RMSE_cos, 'o-')
 hold on
+plot(i, RMSE_pearson, '.-')
+hold on
 plot(i, RMSE_NB, '+-')
 hold on
 plot(i, RMSE_PMF, '*-')
 hold off
-legend('baseline', 'cosine', 'NB', 'PMF')
+legend('baseline', 'cosine', 'Pearson', 'NB', 'PMF')
 str = 'RMSE of Different Algorithms VS Sparsity';
 title(str)
 xlabel('Sparsity')
