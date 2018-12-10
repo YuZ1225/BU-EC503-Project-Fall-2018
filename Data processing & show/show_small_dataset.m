9ff0412837472df1dc6e937f4e2288b60d365cc6
YuZ1225 <yuzhao95@bu.edu>
2018-12-10 02:37:59 -0500

    update show_smalldataset.m

--- Data processing & show/show_small_dataset.m
+++ Data processing & show/show_small_dataset.m
@@ -8,7 +8,7 @@ for i = 1:3:10
     [book_num, user_num] = size(cellofmatrix{i});
     subplot(2,2,j)
     spy(cellofmatrix{i})
-    tmp = sum(sum(cellofmatrix{i}>0))/book_num/user_num;
+    tmp = 1 - sum(sum(cellofmatrix{i}>0))/book_num/user_num;
     title(['Sparsity = ', num2str(tmp)])
     xlabel('User ID')
     ylabel('Book ID')
